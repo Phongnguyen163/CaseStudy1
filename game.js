056735bb7354cdbc4dd6ca4a07647aa0ce3ace84
Phongnguyen163 <thanhphongnguyen99163@gmail.com>
2022-03-30 10:03:26 +0700

    done

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const holes = document.querySelectorAll('.square');
 const timeleft = document.querySelector('#time-left');
 const score = document.getElementById('score');
+const hammer = document.querySelector('.hammer')
 
 let lastHole;
 let Dap;
